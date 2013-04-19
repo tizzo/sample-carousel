fed6962f73556cfee15207706e45606450c8fee2
Howard Tyson <howard@howardtyson.com>
2013-04-19 18:21:31 +0000

    Adding initial list.

--- carousel.js
+++ carousel.js
@@ -8,7 +8,31 @@ Buffer.prototype.getNext = function() {
   return this.unusedList.shift();
 };
 Buffer.prototype.addItems = function(incomingItems) {
-  this.unusedList.concat(incomingItems);
+  this.unusedList = this.unusedList.concat(incomingItems);
 };
 $(document).ready(function() {
+  var initialItems = [
+    {
+      title: 'This is Batman.',
+      image: 'http://images4.wikia.nocookie.net/__cb20120113133430/batman/images/e/e9/000hctfysy2.jpg'
+    },
+    {
+      title: 'This is Superman.',
+      image: 'http://images1.wikia.nocookie.net/__cb20100819014815/superman/images/7/72/Superman.jpg'
+    },
+    {
+      title: 'This is Wonder Woman.',
+      image: 'http://amazingstoriesmag.com/wp-content/uploads/2013/02/wonderwoman.jpg'
+    },
+    {
+      title: 'This is The Flash.',
+      image: 'http://www.hyperborea.org/flash/bigimages/wally5.jpg'
+    },
+    {
+      title: 'This is Green Lantern.',
+      image: 'http://images2.wikia.nocookie.net/__cb20121223023426/bigbangtheory/images/f/ff/Gl.jpg'
+    },
+  ];
+  var buffer = new Buffer(initialItems);
+  console.log(buffer.getNext());
 });
