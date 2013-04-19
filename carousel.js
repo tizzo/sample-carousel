4f6fa7e8200af22c3b51bb491706deeb0dcc14ba
Howard Tyson <howard@howardtyson.com>
2013-04-19 18:15:44 +0000

    Adding initial buffer implementation.

--- carousel.js
+++ carousel.js
@@ -1,3 +1,14 @@
+var Buffer = function(list) {
+  if (list) {
+    this.addItems(list);
+  }
+}
+Buffer.prototype.unusedList = [];
+Buffer.prototype.getNext = function() {
+  return this.unusedList.shift();
+};
+Buffer.prototype.addItems = function(incomingItems) {
+  this.unusedList.concat(incomingItems);
+};
 $(document).ready(function() {
-
 });
